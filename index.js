691c8e70f351dfbd5e2d26f8bd13fe962bc68d31
Louis Jacob <loujacob2@gmail.com>
2021-10-04 00:03:31 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const names = ["Lisa", "Kaitlin", "Jan"];
+
+const writeCards = (names) => {
+    let newArray = [];
+    for(let i = 0; i < names.length; i++) {
+        newArray.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return newArray;
+}
+
+const countDown = (num) => {
+    while(num >= 0) {
+    console.log(num)
+    num --;
+    }
+}
